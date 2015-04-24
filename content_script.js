3cf125304b90d9987feedbd346bd9a51920a0592
Charlie Yang <charlesy@stanford.edu>
2015-04-23 19:53:17 -0700

    catches mark undone events

--- content_script.js
+++ content_script.js
@@ -7,6 +7,19 @@
 
 	var STORAGE_STR = "storage";
 
+	var BODY_SELECTOR = ".cI";
+	var MARK_DONE_SELECTOR = ".itemIconDone";
+	var MARK_UNDONE_SELECTOR = ".itemIconMarkedDone";
+	var SWEEP_SELECTOR= "button.dr";
+	var SWEEP_LIST_SELECTOR = ".DsPmj";
+	var SWEEP_ITEM_SELECTOR = ".scroll-list-item";
+
+	var UNDO_SELECTOR = "span.fD";
+	var UNDO_PARENT_SELECTOR = ".IbRB2e";
+
+
+	var lastLevelIncrease = 0;
+
 
 	function checkLevel(){
 		var level = localStorage.getItem(STORAGE_STR);
@@ -21,40 +34,84 @@
 		return localStorage.getItem(STORAGE_STR);
 	}
 
-	function increaseLevel(){
+	function changeLevel(val){
 		checkLevel();
 		var level = parseInt(localStorage.getItem(STORAGE_STR));
-		level += 1;
+		level += val;
+		lastLevelIncrease = val;
+
 		localStorage.setItem(STORAGE_STR, level);
 	}
 
+	function increaseLevel(amount){
+		var amt = amount == undefined ? 1 : amount;
+
+		checkLevel();
+		changeLevel(amt);
+	}
+
+	function decreaseLevel(amount){
+		var amt = amount == undefined ? 1 : amount;
+
+		checkLevel();
+		changeLevel( -amt );
+	}
+
+
+	function undoLevelChange(){
+		checkLevel();
+		changeLevel( -lastLevelIncrease );
+	}
+
+
 	function bindClickEvents(){
-		$(".scroll-list-item").click(function(event){
-			
+		$(BODY_SELECTOR).on('click', MARK_DONE_SELECTOR, function(event){
+		// $(".scroll-list-item").click(function(event){
 
-			if ($(event.target).hasClass("itemIconDone")){
+			console.log("mark done clicked");
 
-				console.log("single todo clicked");
+			increaseLevel(1);
+			console.log("Level: " + getLevel());
+		})
 
-				increaseLevel();
-				console.log("Level: " + getLevel());
-			}
+		// TODO: will also need to react to Confirm Sweepalls
+		$(BODY_SELECTOR).on('click', SWEEP_SELECTOR, function(event){
 
+			// figures out how many
+			var button = event.target;
+			var parent = $(button.closest(SWEEP_LIST_SELECTOR));
+			var items = parent.find(SWEEP_ITEM_SELECTOR);
+
+			// needs to check if the sweep many appears
+			// there needs to be some way to figure out how many are swept away
+			// probably set up some kind of delay before checking...
+			console.log("sweep all clicked, " + items.size() + " items cleared");
+			increaseLevel(items.size());
+
+			console.log("Level: " + getLevel());	
 		})
 
 
-		$('button.dr').click(function(event){
+		$(BODY_SELECTOR).on('click', MARK_UNDONE_SELECTOR, function(event){
+			console.log("mark undone clicked");
 
-			console.log("sweep all clicked");
+			// undoLevelChange();
+			decreaseLevel(1);
+			console.log("Level: " + getLevel());	
+		})
 
-			increaseLevel();
+		$(UNDO_PARENT_SELECTOR).on('click', UNDO_SELECTOR, function(event){
+			console.log("undo clicked");
+
+			undoLevelChange();
 			console.log("Level: " + getLevel());	
 		})
 
 	}
 
 	function checkEventsBinded(){
-		if ($._data($('.scroll-list-item').get(0), "events") == undefined){
+		if ($._data($('.cI').get(0), "events") == undefined){
+		// if ($._data($('.scroll-list-item').get(0), "events") == undefined){
 			console.log("bind failed");
 			return false;
 		} else {
